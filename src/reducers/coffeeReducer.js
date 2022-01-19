78a0dffa62d1dc500e58d7c1ec554978af02da47
Winifred Lu <winnie.lu@comcast.net>
2022-01-19 13:59:02 -0500

    basic reducer in place

---
+++ src/reducers/coffeeReducer.js
@@ -0,0 +1,11 @@
+
+const initialState = {
+    coffee: []
+};
+
+export const coffeeReducer = (state = initialState, action => {
+    switch (action.type) {
+        default:
+            return state;
+    }
+})
